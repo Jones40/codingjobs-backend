4d15c70930af094aa2df5d796356bf3dc6b96d71
simon <fctinstean@gmail.com>
2021-08-06 14:21:53 +0200

    exercise

--- PHP/Intermediate Evaluation/Solution Rattrapage/rattrapage.php
+++ PHP/Intermediate Evaluation/Solution Rattrapage/rattrapage.php
@@ -4,24 +4,22 @@
     function converter($amount, $currency){
         // You can use gettype, is_int, is_float etc.....
         if(gettype($amount) == 'integer' || gettype($amount) =='double'){
-            if($currency=='YEN' || $currency=='EUR'){
                 switch($currency){
                     case 'YEN':
-
                         echo $amount. 'YEN = '.$amount/129.5. ' EUR<br>';
                         break;
                     case 'EUR':
                         echo $amount. 'EUR = ' . $amount*129.5. ' YEN<br>';
-                       break;
-                }
+                        break;
+                    default:
+                        echo 'currency muss be EUR or YEN<br>'; 
+
             }
             else
                 echo 'currency muss be EUR or YEN<br>'; 
         }
         else
            echo 'Amont muss be a number<br>'; 
-      
- 
     }
 
     // Call/Use the function
