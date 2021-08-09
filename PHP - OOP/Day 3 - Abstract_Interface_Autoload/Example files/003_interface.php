23d14086858d31ff0186d09ddbe22072029e146e
simon <fctinstean@gmail.com>
2021-08-09 09:54:18 +0200

    interface

--- PHP - OOP/Day 3 - Abstract_Interface_Autoload/Example files/003_interface.php
+++ PHP - OOP/Day 3 - Abstract_Interface_Autoload/Example files/003_interface.php
@@ -39,3 +39,25 @@ class Human implements IExample
         echo 'Human is sitting';
     }
 }
+
+/* 
+  Abstract class :
+  1. When a model/template must be set for a group
+  of subclasses, use an abstract class.
+  2. An abstract class can contain concrete methods
+  3. Abstract classes provide default actions for
+  subclasses
+  4. Used to provide a template for potential future
+  inherited classes.
+
+  Interface :
+  1. Use an interface when a role must be defined
+  for other classes as well, regardless of the 
+  inheritance of these classes.
+  2. You can't instantiate an interface 
+  and create an object
+  3. Interface can't contain concrete methods
+  4. A class can implement multiple interfaces
+  (5. An interface can inherit from a class)
+
+
