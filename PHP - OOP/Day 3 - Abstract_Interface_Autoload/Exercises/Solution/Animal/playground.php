05a88e72ec23aa7f590d7ef3a710b7c01c0c8190
Simon <fctinstean@gmail.com>
2021-08-09 12:18:25 +0200

    solution

--- PHP - OOP/Day 3 - Abstract_Interface_Autoload/Exercises/Solution/Animal/playground.php
+++ PHP - OOP/Day 3 - Abstract_Interface_Autoload/Exercises/Solution/Animal/playground.php
@@ -4,12 +4,45 @@ spl_autoload_register();
 
 $garfield = new Cat('Ginger', 'Male', 'Garfield', 4);
 $snoopy = new Dog('Black&White', 'Male', 'Snoopy', 4);
-
-$garfield->communicate();
-$snoopy->communicate();
+$caty = new Cat('Black', 'Female', 'Caty', 3);
+$joe = new Dog('Brown', 'Male', 'Joe the cat', 3);
 
 $simon = new Human('Simon', 'Brown', 'Male');
-$robot = new Robot('z1239', 'Silver');
+$john = new Human('John', 'Brown', 'Male');
 
-$simon->work();
-$robot->work();
+$robot = new Robot('z1239', 'Silver');
+$robot2 = new Robot('y9877', 'Gold');
+
+// Creature array 
+$creatureArray = array(
+    $garfield,
+    $snoopy,
+    $caty,
+    $joe,
+    $simon,
+    new Human('John', 'Brown', 'Male')
+);
+
+// Create Human/robots
+$workersArray = array(
+    $simon,
+    $john,
+    $robot,
+    $robot2
+);
+
+// Loop 10 times
+for ($i = 0; $i <= 10; $i++) {
+    // generate random int
+    $random = rand(0, count($creatureArray) - 1);
+    // Access the object and ask it to communicate
+    $creatureArray[$random]->communicate();
+}
+
+// Loop 10 times
+for ($i = 0; $i <= 10; $i++) {
+    // generate random int
+    $random = rand(0, count($workersArray) - 1);
+    // Access the object and ask it to communicate
+    $workersArray[$random]->work();
+}
